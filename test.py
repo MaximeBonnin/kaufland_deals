d0764a4115047267a91d635b55837889744b8c88
Maxime Bonnin <maxime.bonnin@gmx.de>
2022-10-07 09:39:28 +0200

    test app run

--- test.py
+++ test.py
@@ -12,5 +12,5 @@ def number(num):
     return f"<h1> TEST {num}</h1>"
 
 if __name__ == '__main__':
-    app.run(host="127.0.0.1", port=8080, debug=True)
-    # app.run()
+    # app.run(host="127.0.0.1", port=8080, debug=True)
+    app.run()
