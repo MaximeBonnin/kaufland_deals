e069f04d35115cb80193d7f01f52549ee264449c
Maxime Bonnin <maxime.bonnin@gmx.de>
2022-10-07 09:35:05 +0200

    test

---
+++ test.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, request, redirect
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "<h1>TEST</1>"
+
+
+@app.route("/<num>")
+def number(num):
+    return f"<h1> TEST {num}</h1>"
+
+if __name__ == '__main__':
+    app.run(host="127.0.0.1", port=8080, debug=True)
+    # app.run()
