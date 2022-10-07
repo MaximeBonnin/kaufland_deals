0b11254923bec0e76489d1f799a0f45d4050f803
Maxime Bonnin <maxime.bonnin@gmx.de>
2022-10-07 09:50:42 +0200

    app.py rename

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ def index():
 def number(num):
     return f"<h1> TEST {num}</h1>"
 
+
 if __name__ == '__main__':
-    # app.run(host="127.0.0.1", port=8080, debug=True)
-    app.run()
+    app.run(host="127.0.0.1", port=8080, debug=True)
+    # app.run()
